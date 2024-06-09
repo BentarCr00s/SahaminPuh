79d2a22655e01a35a65ac29f11c142246cb8c605
Ummam Hoerussifa <111702142+Sumems@users.noreply.github.com>
2024-06-09 13:32:32 +0700

    dump

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -5,10 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+    <div class="py-5">
+        <div class="container">
+            <div class="bg-white dark:bg-dark overflow-hidden shadow-sm rounded">
+                <div class="p-4 text-dark dark:text-light">
                     {{ __("Latest news updates will appear here.") }}
                 </div>
             </div>
